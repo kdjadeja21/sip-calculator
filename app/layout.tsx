6caf8b54fd8afa4819872073c3de759ecf3efa27
kdjadeja21 <krushnasinh.jadeja14545@marwadieducation.edu.in>
2024-04-30 17:01:54 +0530

    Modify favicon icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
-import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
