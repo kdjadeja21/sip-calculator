dd2ec5f103d9fe8d1e2d82ca9f7768fc70dd880b
kdjadeja21 <krushnasinh.jadeja14545@marwadieducation.edu.in>
2024-04-30 16:35:49 +0530

    update sitemap file

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,17 +8,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
-    {
-      url: "https://sip-calculation.vercel.app/",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-    {
-      url: "https://calculating-sip.vercel.app/",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
-    },
   ];
 }
